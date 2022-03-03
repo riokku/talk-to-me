6fd7613521b349fb187332b44b39f111ea66893c
Chris Stinson <chrisistinson@gmail.com>
2022-03-03 10:45:32 -0800

    vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
-import vercel from '@sveltejs/adapter-vercel';
+//import adapter from '@sveltejs/adapter-auto';
+//import static_adapter from '@sveltejs/adapter-static';
 
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		//adapter: adapter(),
+		//adapter: static_adapter(),
 		adapter: vercel(),
 
 		// Override http methods in the Todo forms
